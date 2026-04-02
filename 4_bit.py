15a8fb78b85c9afa6b655c87406c89181ff4df73
Rakib121209 <r009rakib@gmail.com>
2026-04-02 15:14:57 +0900

    Add files via upload

---
+++ 4_bit.py
@@ -0,0 +1,46 @@
+a = 132  # 0b10000100
+b = 45   # 0b00101101
+
+fmt0 = '{:<10}'
+fmt1 = '0b{:08b} {:3d}'
+
+n = 30
+
+print("bitwise AND:")
+print(fmt0.format('a'), fmt1.format(a, a))
+print(fmt0.format('b'), fmt1.format(b, b))
+print('_' * n)
+print(fmt0.format('a&b'), fmt1.format(a & b, a & b))
+
+
+
+print("bitwise OR:")
+print(fmt0.format('a'), fmt1.format(a, a))
+print(fmt0.format('b'), fmt1.format(b, b))
+print('_' * n)
+print(fmt0.format('a|b'), fmt1.format(a | b, a | b))
+
+print("bitwise XOR:")
+print(fmt0.format('a'), fmt1.format(a, a))
+print(fmt0.format('b'), fmt1.format(b, b))
+print('_' * n)
+print(fmt0.format('a ^ b'), fmt1.format(a ^ b, a ^ b))
+
+print("bitwise leftshift")
+print(fmt0.format('a'), fmt1.format(a, a))
+print('_' * n)
+print(fmt0.format('~a'), fmt1.format(~a & 0xFF, ~a & 0xFF))
+
+print("bitwise leftshift")
+print(fmt0.format('a'), fmt1.format(a, a))
+print('_' * n)
+print(fmt0.format('a<<2'), fmt1.format(a<<2&0xFF, a<<2 & 0xFF))
+
+print("bitwise leftshift")
+print(fmt0.format('a'), fmt1.format(a, a))
+print('_' * n)
+print(fmt0.format('a>>2'), fmt1.format(a>>2&0xFF, a>>2 & 0xFF))
+
+
+
+
